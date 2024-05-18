6dcf28ed0f4ff9d4739691e912f08002d5f07653
MaraveliasGiannhs <118015594+MaraveliasGiannhs@users.noreply.github.com>
2024-05-18 20:48:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 document.addEventListener('DOMContentLoaded', function(){
     const button = document.getElementById('addUserButton');
+    const contact = document.getElementById("contactCard");
 
+    var show = false;
     button.addEventListener('click', function(){
-
-        if (button.classList.contains('hover-style')) 
+        show = !show;
+        if (show) 
         {
-            button.classList.remove('hover-style');
-            button.classList.add('active-style');
-            button.value="";
+            
+            button.value="Cancel";
+            contact.classList.remove('hide');
+            contact.style.display = "block";
+            contact.classList.add('show');
         } 
         else 
-        {
-            button.classList.remove('active-style');
-            button.classList.add('hover-style');
+        {            
             button.value="Add New Contact";
+            contact.classList.remove('show');
+            contact.classList.add('hide');
+            contact.style.display = "none";
+
+            setTimeout(() => {
+                if (!show) 
+                { // Check the state to ensure it's still hidden
+                    contact.style.display = "none";
+                }
+            }, 510);
         }
+
+        
     })
 })
 
